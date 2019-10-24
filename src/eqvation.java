102e071db7f679a95334c6951309657ec9cb86df
MilkyasEyob.Habte <milkyas23>
2019-10-24 11:13:12 +0200

    Addition code

--- src/eqvation.java
+++ src/eqvation.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class eqvation {
+    public static void main(String[] args) {
+        int i;     // en int-vairiabel med namnet i
+        double d;  // double-vairiabel med namnet d
+        double summa; // double-vairiabel med namnet s
+
+        i = 6;
+        d = 12;
+        summa = i+d;
+        JOptionPane.showMessageDialog(null, "summan är"  +summa);
+
+
+
+
+
+
+    }
 }
